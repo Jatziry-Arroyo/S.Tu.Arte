4b805d2e0e99667d0e964dfed9cbaf6e0bdb1597
AzucenaPA <m4o1800o2423c@mooc-cuaed.unam.mx>
2024-05-23 23:51:54 -0600

    Validación formulario RC sin fecha nacimiento

--- js/registroCompradores.js
+++ js/registroCompradores.js
@@ -51,13 +51,66 @@ document.getElementById("form-registro-comprador").addEventListener("submit", fu
     event.preventDefault();
 
     var numOptionPais = document.getElementById("pais");
+    var selectedOption = numOptionPais.options[numOptionPais.selectedIndex].text;
     var estado = document.getElementById("estado").value.trim();
-    var ciudad = docuemnt.getElementById("ciudad").value.trim();
-    var domiciolio = document.getElementById("domicilio").value.trim();
-    var fechanacimiento = document.getElementById("fechanacimiento").value();
-    /*
-        var domicilioRegex = ;
-        var edoCdRegex = ; 
-        */
-})
+    var ciudad = document.getElementById("ciudad").value.trim();
+    var domicilio = document.getElementById("domicilio").value.trim();
+    var fechanacimiento = document.getElementById("fechanacimiento");
 
+    var domicilioRegex = /^[a-zA-Z0-9áéíóúÁÉÍÓÚüÜñÑ#\-\. ]+$/;
+    var edoCdRegex = /^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\-\ ]+$/;
+
+    var paisError = '';
+    var domicilioError = "";
+    var edoError = "";
+    var cdError = "";
+
+    if (selectedOption == "Opciones") {
+        var paisError = "Necesitas elegir una opción";
+        document.getElementById("pais").classList.add("input-error");
+    }
+
+    if (!edoCdRegex.test(estado)) {
+        var edoError = "Por favor inserta un estado que no contenga números";
+        document.getElementById("estado").classList.add("input-error");
+    }
+
+    if (!edoCdRegex.test(ciudad)) {
+        var cdError = "Por favor inserta una ciudad que no contenga números";
+        document.getElementById("ciudad").classList.add("input-error");
+    }
+
+    if (!domicilioRegex.test(domicilio) && domicilio.length < 10) {
+        var domicilioError = "Asegúrate de que la dirección tenga suficientes caracteres y estos sean necesarios para una dirección";
+        document.getElementById("domicilio").classList.add("input-error");
+    }
+
+    document.getElementById("paisError").textContent = paisError;
+    document.getElementById("edoError").textContent = edoError;
+    document.getElementById("cdError").textContent = cdError;
+    document.getElementById("domicilioError").textContent = domicilioError;
+
+    if (paisError || edoError || cdError || domicilioError) {
+        return;
+    } else {
+        const dataComprador = {
+            "pais": selectedOption,
+            "estado": estado,
+            "ciudad": ciudad,
+            "domicilio": domicilio,
+            "fecha-nacimiento": fechanacimiento,
+        }
+        const stringifyData = JSON.stringify(dataComprador);
+        console.log(stringifyData);
+    }
+});
+//es necesario lo de abajo porque si no, no se muestran los mensajes de error
+//document.addEventListener("DOMContentLoaded", function () {
+document.getElementById("form-registro-comprador").addEventListener("input", function () {
+    document.getElementById("errorMessages").innerHTML = "";
+    document.getElementById("pais").classList.remove("input-error");
+    document.getElementById("estado").classList.remove("input-error");
+    document.getElementById("ciudad").classList.remove("input-error");
+    document.getElementById("domicilio").classList.remove("input-error");
+});
+//})
