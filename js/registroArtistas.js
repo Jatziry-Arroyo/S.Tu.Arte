cc4e8b5d61c127e55e9086b6a69214b3449c0a2b
Jatziry-Arroyo <ln.jatziryarroyo@gmail.com>
2024-05-24 22:41:42 -0600

    Formulario de registro para artistas

---
+++ js/registroArtistas.js
@@ -0,0 +1,23 @@
+'use strict'
+const nacimiento=document.querySelector('#nacimiento');
+const alias=document.querySelector('#alias');
+const facultad=document.querySelector('#facultad');
+//const añoescolar=document.querySelector('#añoescolar');
+const tecnicas=document.querySelector('#tecnicas');
+const reseña=document.querySelector('#reseña');
+
+function goBack() {
+    window.history.back();
+}
+
+document.getElementById("btn-guardar").addEventListener("click", function() {
+    if (document.getElementById("form-registro-artistx").checkValidity()) {
+        alert("Formulario válido. Datos guardados correctamente.");
+    } else {
+        alert("Por favor, completa todos los campos correctamente.");
+    }
+});
+
+function goBack() {
+    window.history.back();
+}
