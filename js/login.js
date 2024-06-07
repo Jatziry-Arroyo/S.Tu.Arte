f1435a3e87d41c5349fbe4561460cba74e31e637
AngeHernandezJ <angelica.hernandez.jaimes01@gmail.com>
2024-06-07 09:23:53 -0600

    cambios en login

--- js/login.js
+++ js/login.js
@@ -2,8 +2,10 @@ const loginForm = document.getElementById('loginForm');
 const emailInput = document.getElementById('userEmail');
 const passwordInput = document.getElementById('password');
 const alertContainer = document.getElementById('alert-container');
+const btnEnviar =document.getElementById('btn-rosa');
+const btnCerrar =document.getElementById('btn-rosa2');
 
-loginForm.addEventListener('submit', function (event) {
+btnEnviar.addEventListener('click', function (event) {
     event.preventDefault();
     event.stopPropagation();
 
@@ -34,7 +36,7 @@ loginForm.addEventListener('submit', function (event) {
         loginForm.reportValidity();
     } else {
 
-        // Crear objeto usuario
+       /* // Crear objeto usuario
         let usuario = {
             'useremail': emailInput.value,
             'userpassword': passwordInput.value
@@ -49,7 +51,8 @@ loginForm.addEventListener('submit', function (event) {
 
         // Reinicia el formulario
         loginForm.reset();
-        loginForm.classList.remove('was-validated');
+        loginForm.classList.remove('was-validated');*/
+        validateUser();
 
     }
 });
@@ -70,3 +73,77 @@ passwordInput.addEventListener('input', () => {
     passwordInput.setCustomValidity('');
     passwordInput.checkValidity();
 });
+
+fetch("../package/users.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error al cargar el archivo JSON");
+        }
+        return response.json();
+    })
+    .then(data => {
+        usuarios = data;
+        //cargarProductosCarrito(productos);
+    })
+    .catch(error => console.error("Error en la solicitud fetch:", error));
+
+
+function validateUser() {
+    let usuarioEncontrado = usuarios.find(usuario => usuario.mail === (emailInput.value));
+    console.log(emailInput.value);
+    if (usuarioEncontrado) {
+        console.log("Existe");
+        //console.log(usuarioEncontrado.mail);
+        if (usuarioEncontrado.password === passwordInput.value) {
+            console.log("Usuario validado");
+            console.log(usuarioEncontrado.type)
+            // Crear objeto usuario
+            /*let usuario = {
+                'useremail': emailInput.value,
+                'userpassword': passwordInput.value
+            };*/
+
+            // Guardar objeto usuario en localStorage como JSON
+            localStorage.setItem("Usuario", usuarioEncontrado.type);
+
+            // Aquí se enviaría el formulario para iniciar sesión.
+            console.log('Formulario válido. Se enviaría el formulario.');
+            //console.log(usuario);
+
+            // Reinicia el formulario
+            loginForm.reset();
+            loginForm.classList.remove('was-validated');
+
+            btnEnviar.classList.add("disabled");
+            btnCerrar.classList.remove("disabled");
+
+        }
+        else {
+            console.log("Contraseña incorrecta");
+            alert("Contraseña incorrecta");
+        }
+    }
+    else {
+        console.log("Ese usuario no existe");
+        alert("Ese correo no está registrado. Por favor verifica tus datos de usuario");
+    }
+}
+
+function enviarCerrar(){
+    
+    if(localStorage.getItem("Usuario")){
+        btnEnviar.classList.add("disabled");
+        btnCerrar.classList.remove("disabled");
+    }
+    else {
+        btnEnviar.classList.remove("disabled");
+            btnCerrar.classList.add("disabled");
+    }
+}
+
+btnCerrar.addEventListener('click', function (event) {
+    localStorage.removeItem("Usuario");
+    enviarCerrar();
+});
+
+window.addEventListener("storage",enviarCerrar());
