3dea9f955b2b638fa8a5d7db4703e6c5ed0f00a9
AngeHernandezJ <angelica.hernandez.jaimes01@gmail.com>
2024-06-10 21:52:32 -0600

    Filtro del catalogo en todas las paginas, HTML, JS. Excepto TalentoMexa

--- js/acerca.js
+++ js/acerca.js
@@ -1,4 +1,4 @@
-let imgBx = document.querySelectorAll(".imgBx");
+/*let imgBx = document.querySelectorAll(".imgBx");
 let contentBx = document.querySelectorAll(".contentBx");
 
 for (var i = 0; i < imgBx.length; i++) {
@@ -13,4 +13,29 @@ for (var i = 0; i < imgBx.length; i++) {
         }
         this.className = "imgBx active";
     })
+}*/
+
+// Actualizar nùmero carrito
+function actualizarNumeroCarrito() {
+    const productosEnCarrito = localStorage.getItem("productosEnCarrito");
+    const contenedorNumCarrito = document.querySelector("#numerito");
+    contenedorNumCarrito.innerHTML = productosEnCarrito;
 }
+window.addEventListener('storage', actualizarNumeroCarrito);
+actualizarNumeroCarrito();
+
+// Guardando la tècnica para filtrar
+const pageGrabado = document.getElementById('pageGrabado');
+pageGrabado.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Grabado')});
+const pageEscultura = document.getElementById('pageEscultura');
+pageEscultura.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Escultura')});
+const pageOleo = document.getElementById('pageOleo');
+pageOleo.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Óleo')});
+const pageCollage = document.getElementById('pageCollage');
+pageCollage.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Collage')});
+const pageAcuarela = document.getElementById('pageAcuarela');
+pageAcuarela.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Acuarela')});
+const pageFotografia = document.getElementById('pageFotografia');
+pageFotografia.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Fotografía')});
+const pageAcrilico = document.getElementById('pageAcrilico');
+pageAcrilico.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Acrílico')});
--- js/registroCompradores.js
+++ js/registroCompradores.js
@@ -58,3 +58,28 @@ birthday.addEventListener("input", function () {
 function goBack() {
     window.history.back();
 }
+
+// Actualizar nùmero carrito
+function actualizarNumeroCarrito() {
+    const productosEnCarrito = localStorage.getItem("productosEnCarrito");
+    const contenedorNumCarrito = document.querySelector("#numerito");
+    contenedorNumCarrito.innerHTML = productosEnCarrito;
+}
+window.addEventListener('storage', actualizarNumeroCarrito);
+actualizarNumeroCarrito();
+
+// Guardando la tècnica para filtrar
+const pageGrabado = document.getElementById('pageGrabado');
+pageGrabado.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Grabado')});
+const pageEscultura = document.getElementById('pageEscultura');
+pageEscultura.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Escultura')});
+const pageOleo = document.getElementById('pageOleo');
+pageOleo.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Óleo')});
+const pageCollage = document.getElementById('pageCollage');
+pageCollage.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Collage')});
+const pageAcuarela = document.getElementById('pageAcuarela');
+pageAcuarela.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Acuarela')});
+const pageFotografia = document.getElementById('pageFotografia');
+pageFotografia.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Fotografía')});
+const pageAcrilico = document.getElementById('pageAcrilico');
+pageAcrilico.addEventListener('click', function(){localStorage.setItem('Tecnica', 'Acrílico')});
